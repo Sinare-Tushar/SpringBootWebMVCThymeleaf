9fe3f128b53c8fe6746e1c0ff2c01551b9456549
Sinare-Tushar <tusharsinare020295@gmail.com>
2023-05-27 12:22:30 +0530

    Add Changes

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -5,33 +5,34 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.app.model.Employee;
 
 @Controller
-@RequestMapping("emp")
 public class EmployeeController {
-	@RequestMapping("reg")
+	@GetMapping("reg")
 	public String regPage(ModelMap map) {
 		Employee emp = new Employee();
 		map.addAttribute("employee", emp);
 		return "Register";
 	}
 
-	@RequestMapping(value = "save", method = RequestMethod.POST)
+	@PostMapping(value = "save")
 	public String saveData(@ModelAttribute Employee employee, ModelMap map) {
 		map.addAttribute("emp", employee);
 		return "Info";
 	}
 
-	@RequestMapping("all")
-	public String showDates(ModelMap map) {
+	@GetMapping("all")
+	public String showDates( ModelMap map) {
 		map.addAttribute("message", "Hello UI");
-		List<Employee> emps = Arrays.asList(new Employee(), new Employee(), new Employee());
+		List<Employee> emps = Arrays.asList(new Employee());
 		map.addAttribute("list", emps);
-		return "Data";
+		return "Demo";
 	}
 }
--- src/test/java/com/app/SpringBootWebMVCThymeleaf/SpringBootWebMvcThymeleafApplicationTests.java
+++ src/test/java/com/app/SpringBootWebMVCThymeleaf/SpringBootWebMvcThymeleafApplicationTests.java
@@ -3,11 +3,11 @@ package com.app.SpringBootWebMVCThymeleaf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.app.model.Employee;
+
 @SpringBootTest
 class SpringBootWebMvcThymeleafApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
